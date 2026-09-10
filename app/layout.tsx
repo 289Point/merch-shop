6c8edb7a4c789198d1f11b8f5cd596bc8d46c6f5
289Point <enricolonghi2323@gmail.com>
2026-09-10 02:53:23 +0200

    Update title in layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 
 export const metadata: Metadata = {
-  title: "Catalogo",
+  title: "289Point Showroom",
   description: "Vetrina prodotti",
 };
 
